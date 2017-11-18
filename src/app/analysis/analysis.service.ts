f553b95167d1948fb29b01a68969503bfecb5454
pavelcreator <pavelcreator@gmail.com>
2017-11-18 13:01:10 +0200

    Regex pattern

--- src/app/analysis/analysis.service.ts
+++ src/app/analysis/analysis.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AnalysisService {
+
+  pattern: RegExp = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/;
+
   analyzeUrl(url: string): boolean {
-    const result = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/.test(url);
+    const result = this.pattern.test(url);
     return result;
   }
+
   parseUrl(url: string): string {
-    const parseArr = url.match( /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/ );
+    const parseArr = url.match(this.pattern);
     const videoId = parseArr[1];
     return videoId;
   }
